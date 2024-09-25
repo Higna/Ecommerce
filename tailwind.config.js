a1193760f08ffd4da0b1db060531b9fc062f73b5
Hector <52350216+Higna@users.noreply.github.com>
2024-09-25 10:16:46 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "black": "#1E2832"
+      }
+    },
   },
   plugins: [],
 }
